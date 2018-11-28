60d38792fc74e3eac5e684209605aa35ecfe3ee5
frezzatod <denis.frezzato@key-one.it>
2018-11-28 17:14:50 +0100

    Chapter 5: named patters.

--- exercises/chapter5/src/Exercises.purs
+++ exercises/chapter5/src/Exercises.purs
@@ -12,3 +12,14 @@ binomialCoefficients :: Int -> Int -> Int
 binomialCoefficients 0 _ = 1
 binomialCoefficients 1 _ = 1
 binomialCoefficients n k = fact n / (fact k * fact (n - k))
+
+type Address = { street :: String, city :: String }
+
+type Person = { name :: String, address :: Address }
+
+sameCity :: Person -> Person -> Boolean
+sameCity { address: { city: cityA } } { address: { city: cityB } } = cityA == cityB
+
+fromSingleton :: forall a. a -> Array a -> a
+fromSingleton _ [a] = a
+fromSingleton a _ = a
