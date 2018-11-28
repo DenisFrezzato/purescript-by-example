ca80bb12ed2488e155bc2d8c89f73c6c4f042bc7
frezzatod <denis.frezzato@key-one.it>
2018-11-28 16:04:38 +0100

    Chapter 4: files functions.

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -2,8 +2,10 @@ module FileOperations where
 
 import Prelude
 
-import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Control.MonadZero (guard)
+import Data.Path (Path, ls, isDirectory, size, filename, root)
+import Data.Array (concatMap, (:), filter, foldl, head)
+import Data.Maybe (Maybe(..))
 
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
@@ -12,3 +14,29 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles = filter (not isDirectory) <<< allFiles'
+
+largest :: Path -> Maybe Path
+largest = foldl largest' Nothing <<< onlyFiles
+  where
+    largest' Nothing f = Just f
+    largest' (Just acc) f = if size f > size acc
+      then Just f
+      else Just acc
+
+smallest :: Path -> Maybe Path
+smallest = foldl smallest' Nothing <<< onlyFiles
+  where
+    smallest' Nothing f = Just f
+    smallest' (Just acc) f = if size f < size acc
+      then Just f
+      else Just acc
+
+whereIs :: String -> Maybe Path
+whereIs filePath = head $ do
+  file <- allFiles' root
+  child <- ls file
+  guard $ filename child == filePath
+  pure file
